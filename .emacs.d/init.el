43ea4695a43fd42fab823cb2cc29311964db581f
Valentin Ignatyev <valentjedi@gmail.com>
2017-06-20 01:29:05 +1000

    indentation for js2-mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -178,6 +178,7 @@
   :config
   (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
   (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
+  (setq js2-basic-offset 2)
 
   (use-package rjsx-mode
     :ensure t))
