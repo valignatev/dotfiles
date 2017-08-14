6847e2780a25eab4967753c593ea4c70893ffd3e
Valentin Ignatyev <valentjedi@gmail.com>
2017-08-14 14:56:58 +1000

    reconfigure polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,6 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null;do sleep 1; done
 
-polybar example &
+polybar top &
 
 echo "Polybar launched..."
