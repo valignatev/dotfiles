cad896222fbcf64943ff9cf9929d8474acdde484
Valentin Ignatyev <valentjedi@gmail.com>
2017-10-27 03:42:27 +1000

    add file to agenda

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -3,9 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (yaml-mode web-mode virtualenvwrapper use-package rjsx-mode projectile popup org-bullets markdown-mode flycheck flx evil-surround evil-smartparens evil-leader counsel company-tern company-anaconda color-theme async))))
+ '(org-agenda-files (quote ("~/me.org"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
