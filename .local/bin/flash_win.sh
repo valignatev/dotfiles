abccf18b98097e2289a744845eca520361c16abb
Valentin Ignatyev <valentjedi@gmail.com>
2017-12-19 17:10:07 +1000

    flash not that intense

--- .local/bin/flash_win.sh
+++ .local/bin/flash_win.sh
@@ -2,11 +2,11 @@
 
 # Flashes an active window.
 
-transset-df -a -m 0
+transset-df -a -m 0 0.9
 sleep .1
 transset-df -a -x 1
 sleep .1
-transset-df -a -m 0
+transset-df -a -m 0 0.9
 sleep .1
 transset-df -a -x 1
 sleep .1
