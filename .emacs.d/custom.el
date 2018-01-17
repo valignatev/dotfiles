3f84deb3c3f939f5eb27d0dafded18539d8f3ad4
Valentin Ignatyev <valentjedi@gmail.com>
2018-01-17 12:44:34 +1000

    set default font to monospace 12

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -11,4 +11,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(default ((t (:inherit nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 120 :width normal :foundry "PfEd" :family "monospace")))))
