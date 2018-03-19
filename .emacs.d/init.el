0b662aef9156404190945bcb65dcf83fe8a2d154
Valentin Ignatev <valentjedi@gmail.com>
2018-03-19 13:06:09 +1000

    emacs config: start over because I have no life

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,299 +1,184 @@
-;;; init.el -- My emacs configuration
-;; -*-Emacs-Lisp-*-
-;;; Commentary:
-;; Why linter wants me to do this?
-;;
-;;; Code:
-
-;; My custom-file.
-(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file)
-
-;; Essential settings.
+;; Interface
 (setq inhibit-startup-message t
-      inhibit-splash-screen t
-      inhibit-startup-echo-area-message t)
+      inhibit-splash-screen t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
-(when (boundp 'scroll-bar-mode)
-  (scroll-bar-mode -1))
+(scroll-bar-mode -1)
 (show-paren-mode 1)
-(setq column-number-mode t)
+(setq column-number-mode 1)
 (blink-cursor-mode 0)
-(desktop-save-mode 1)
 (global-visual-line-mode 1)
 (setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
-
-
-;; Store backups and autosaves in temp directory
-(setq backup-directory-alist
-      `((".*" . ,temporary-file-directory)))
-(setq auto-save-file-name-transforms
-      `((".*" ,temporary-file-directory t)))
-
-;; When point goes outside the window, Emacs recenters the buffer point.
-;; This changes scrolling behaviour to only as far as point goes
-(setq scroll-conservatively 101)
-
-;; Highlight the current line
-(when window-system
-  (global-hl-line-mode))
-
-;; No tabs
-(setq-default indent-tabs-mode nil)
-;; But I won't mess current file indentation
-(defun infer-indentation-style ()
-  "Figuring out current indentation type and stick with it."
-  (let ((space-count (how-many "^ " (point-min) (point-max)))
-        (tab-count (how-many "^\t" (point-min) (point-max))))
-    (if (> space-count tab-count) (setq indent-tabs-mode nil))
-    (if (> tab-count space-count) (setq indent-tabs-mode t))))
-
-
-;; Shorten yes or no.
 (defalias 'yes-or-no-p 'y-or-n-p)
 
-;; Delete trailing whitespaces before saving
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
+;; Bind meta to left command key and then unset Command+Space
+;; because I use it for switching layout
+(setq x-super-keysym 'meta)
+(global-unset-key (kbd "M-SPC"))
+(setq scroll-conservatively 101)
+;; https://emacs.stackexchange.com/q/28736/13740
+;; Also, jwiegley does thi s
+(setq auto-window-vscroll nil)
 
-(setq require-final-newline t)
-;; When something changes a file, automatically refresh the buffer containing that file.
+;; Straight package manager bootstrap and setup
+(let ((bootstrap-file (concat user-emacs-directory "straight/repos/straight.el/bootstrap.el"))
+      (bootstrap-version 3))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; Set frame font
+(setq vj/font-name "Hack"
+      vj/font-size 12)
+
+(defun vj/set-frame-font-size (&optional font-size)
+  "Sets font size for all frames. Default is vj/font-size"
+  (interactive (list
+		(read-number "number: " vj/font-size)))
+  (let ((font-size (or font-size vj/font-size)))
+    (set-frame-font (format "%s %d" vj/font-name font-size) nil t)))
+
+(vj/set-frame-font-size)
+
+;; Theme
+;; TODO may be check if custom themes is empty and then load?
+(straight-use-package 'spacemacs-theme)
+(setq spacemacs-theme-comment-bg nil)
+(setq spacemacs-theme-comment-italic t)
+;; If I want to use original comment color with italics together,
+;; I should uncomment these 2 lines.
+;; See https://github.com/nashamri/spacemacs-theme/issues/104
+;; (custom-set-variables '(spacemacs-theme-custom-colors
+;; 			'((comment-light . "#2aa1ae"))))
+(global-hl-line-mode t)
+
+(defvar vj/themes
+  '((light . #'spacemacs-light)
+    (dark . #'spacemacs-dark)))
+
+(defvar vj/theme-type 'light)
+
+(defun vj/toggle-theme ()
+  (interactive)
+  (mapcar #'disable-theme custom-enabled-themes)
+  (if (eq vj/theme-type 'light)
+      (setq vj/theme-type 'dark)
+    (setq vj/theme-type 'light))
+    (load-theme (eval (cdr (assoc vj/theme-type vj/themes))) t))
+
+;; Reset to default emacs theme
+(global-set-key (kbd "C-c <f6>") (lambda ()
+				   (interactive)
+				   (mapcar #'disable-theme custom-enabled-themes)))
+
+(global-set-key (kbd "<f6>") 'vj/toggle-theme)
+(load-theme (eval (cdr (assoc vj/theme-type vj/themes))) t)
+
+;; Buffers and backups
+(desktop-save-mode t)
 (global-auto-revert-mode t)
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq undo-tree-auto-save-history t)
+(setq undo-tree-history-directory-alist
+      `((".*" . ,(concat user-emacs-directory "undo/"))))
 
-;; Package stuff
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-(package-install-selected-packages)
+;; Security
+(setq gnutls-verify-error t)
+(setq tls-checktrust t)
+
+;; Minibuffer (find-file, M-x etc)
+(ido-mode 1)
+(setq ido-enable-flex-matching t)
+(straight-use-package 'ido-vertical-mode)
+(ido-vertical-mode t)
+(setq ido-vertical-define-keys 'C-n-and-C-p-only)
+(straight-use-package 'smex)
+(global-set-key (kbd "M-x") 'smex)
 
 ;; Evil
-(use-package evil
-  :ensure t
-  :init
-  (setq evil-want-C-u-scroll t)
-  :config
-  (evil-mode 1)
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
-  (modify-syntax-entry ?_ "w")
-
-  (use-package evil-leader
-    :ensure t
-    :config
-    (global-evil-leader-mode)
-    (evil-leader/set-leader "<SPC>")
-    (evil-leader/set-key
-      "<SPC>" 'counsel-M-x)
-    )
-
-  (use-package evil-surround
-    :ensure t
-    :config
-    (global-evil-surround-mode)))
-
-
-;; Use Evil motions in occur-mode
-(add-hook 'occur-mode-hook
-          (lambda ()
-            (evil-add-hjkl-bindings occur-mode-map 'emacs
-              (kbd "/") 'evil-search-forward
-              (kbd "?") 'evil-search-backward
-              (kbd "n") 'evil-search-next
-              (kbd "N") 'evil-search-previous
-              (kbd "C-d") 'evil-scroll-down
-              (kbd "C-u") 'evil-scroll-up
-              (kbd "C-w C-w") 'other-window)))
-
-(use-package which-key
-  :ensure t
-  :init
-  (which-key-mode))
-
-;; Sorting engine. Used by swiper and counsel
-(use-package flx
-  :ensure t)
-
-;; Ivy
-(use-package ivy
-  :ensure t
-  :config
-  (setq ivy-use-virtual-buffers t
-        ivy-count-format "%d/%d"
-        ivy-re-builders-alist '((t . ivy--regex-fuzzy)))
-  (ivy-mode )
-
-  (use-package swiper
-    :ensure t)
-
-  (use-package counsel
-    :ensure t
-    :config
-    (global-set-key (kbd "M-x") 'counsel-M-x)
-    (global-set-key (kbd "C-x C-f") 'counsel-find-file))
-  )
-
-;; PATH from shell
-(use-package exec-path-from-shell
-  :disabled t
-  :if (memq window-system '(mac ns))
-  :config
-  (setq exec-path-from-shell-check-startup-files nil
-        exec-path-from-shell-variables '("PATH"))
-  (exec-path-from-shell-initialize))
-
-;; Org-mode
-(use-package org-bullets
-  :ensure t
-  :config (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1))))
-
-(add-hook 'org-mode-hook (lambda () (setq org-src-fontify-natively t)))
-(add-hook 'org-mode-hook (lambda ()
-                           (org-babel-do-load-languages
-                            'org-babel-load-languages
-                            '((python . t)))
-                           ))
-(setq org-ellipsis "↴")
-
-
-(add-hook 'org-mode-hook 'flyspell-mode)
-
-;; Flycheck
-(use-package flycheck
-  :ensure t
-  :config
-  (add-hook 'after-init-hook 'global-flycheck-mode)
-
-  (add-hook 'flycheck-mode-hook
-            (lambda ()
-              (evil-define-key
-                'normal flycheck-mode-map (kbd "]e")
-                'flycheck-next-error)
-              (evil-define-key
-                'normal flycheck-mode-map (kbd "[e")
-                'flycheck-previous-error))
-            ))
-
-;; Markdown
-(use-package markdown-mode
-  :ensure t)
-
-;; Web-mode stuff
-;; File type overrides for web-mode
-(add-to-list 'auto-mode-alist '("\\.html$" . web-mode))
-
-(use-package web-mode
-  :ensure t
-  :defer t
-  :init
-  (setq web-mode-engines-alist
-        '(("django" . "\\.html\\'")
-          ("django" . "\\.jinja2\\'")))
-  (setq web-mode-enable-auto-quoting t)
-  :config
-  (setq web-mode-attr-indent-offset 2
-        web-mode-code-indent-offset 2
-        web-mode-css-indent-offset 2
-        web-mode-indent-style 2
-        web-mode-markup-indent-offset 2
-        web-mode-sql-indent-offset 2)
-
-  (add-hook 'web-mode-hook
-            (lambda ()
-              (setq web-mode-style-padding 2)
-              (flycheck-mode))))
-
-;; Javascript
-(use-package js2-mode
-  :ensure t
-  :config
-  (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
-  (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
-  (setq js2-basic-offset 2)
-
-  (use-package rjsx-mode
-    :ensure t))
-
-;; Company
-(use-package company
-  :ensure t
-  :defer t
-  :init (global-company-mode)
-  :config
-  (setq company-minimum-prefix-length 1)
-  (setq company-idle-delay 0.2)
-  (define-key company-active-map (kbd "C-n") 'company-select-next)
-  (define-key company-active-map (kbd "C-p") 'company-select-previous)
-  (define-key company-search-map (kbd "C-n") 'company-select-next)
-  (define-key company-search-map (kbd "C-p") 'company-select-previous)
-  (setq company-frontends '(company-pseudo-tooltip-frontend
-                            company-echo-metadata-frontend))
-  (use-package company-tern
-    :ensure t
-    :config
-    (add-to-list 'company-backends 'company-tern)))
-
-;; Python stuff
-(use-package anaconda-mode
-  :ensure t
-  :config
-  (add-hook 'python-mode-hook 'anaconda-mode)
-  (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
-  (add-to-list 'company-backends #'company-anaconda)
-
-  (use-package company-anaconda
-    :ensure t
-    :config
-    (eval-after-load "company"
-      '(add-to-list 'company-backends 'company-anaconda))
-    )
-  )
-
-(use-package virtualenvwrapper
-  :ensure t
-  :config
-  (venv-initialize-interactive-shells)
-  (venv-initialize-eshell)
-  (setq venv-location "~/.virtualenvs/"))
-
-;; Smartparens
-(use-package smartparens-config
-  :ensure smartparens
-  :init
-  (add-hook 'prog-mode-hook #'smartparens-mode)
-
-  (use-package evil-smartparens
-    :ensure t
-    :init
-    (add-hook 'smartparens-enabled-hook #'evil-smartparens-mode)))
-
-;; Projectile
-(use-package projectile
-  :ensure t
-  :config
-  (projectile-mode)
-  (setq projectile-completion-system 'ivy)
-  (evil-leader/set-key
-    "f" 'projectile-find-file)
-)
-
-;; Ag
-(use-package ag
-  :ensure t)
-
-;; YAML
-(use-package yaml-mode
-  :ensure t
-  :config
-  (add-hook 'yaml-mode-hook
-            (lambda ()
-              (define-key yaml-mode-map "\C-m" 'newline-and-indent))))
+(straight-use-package 'evil)
+;; Bind it to something silly since I use C-z to run terminal
+(setq evil-vsplit-window-right t)
+(setq evil-split-window-below t)
+;; (setq evil-split-window-below t)
+(setq evil-toggle-key "C-c C-z")
+(setq evil-want-C-u-scroll t)
+(evil-mode 1)
+(modify-syntax-entry ?_ "w")
+(straight-use-package 'evil-surround)
+(global-evil-surround-mode t)
+(straight-use-package 'evil-ediff)
+(evil-ediff-init)
+(straight-use-package 'evil-leader)
+(global-evil-leader-mode)
+(evil-leader/set-leader "<SPC>")
+;; Generalize it
+(evil-leader/set-key
+  "t" '(lambda () (interactive)(shell-command "npm test")))
 
 ;; Magit
-(use-package magit
-  :ensure t
-  :bind ("C-x g" . magit-status))
+(straight-use-package 'magit)
+(global-set-key (kbd "C-c g") 'magit-status)
+(straight-use-package 'evil-magit)
+(evil-magit-init)
 
-(provide 'init)
-;;; init.el ends here
+;; Projectile
+(straight-use-package 'projectile)
+(projectile-mode 1)
+
+;; Modeline
+;; TODO: moody master now autoloads commands
+;;(straight-use-package 'moody)
+;;(require 'moody )
+;; (require 'moody "~/.emacs.d/straight/repos/moody/moody.el")
+;;(setq x-underline-at-descent-line t)
+;;(moody-replace-mode-line-buffer-identification)
+;;(moody-replace-vc-mode)
+(straight-use-package 'minions)
+(minions-mode t)
+
+;; Shell (if I accidentally end up there)
+(setq comint-prompt-read-only t)
+(setq comint-input-ignoredups t)
+
+;; Parse ANSI escape characters in *Shell Command Output* buffer
+(defun parse-ansi-for-shell-command-output (message-or-buffer)
+  (let ((buf message-or-buffer))
+    (and (bufferp buf)
+	 (string= (buffer-name buf) "*Shell Command Output*")
+	 (with-current-buffer buf
+	   (ansi-color-apply-on-region (point-min) (point-max))))))
+(advice-add 'display-message-or-buffer :before #'parse-ansi-for-shell-command-output)
+
+;; Term (I actually use it).
+;; This section is a total mess
+;; Set normal mote for terminal-mode so we can have
+;; block cursor. May be there is a better way
+(evil-set-initial-state 'term-mode 'normal)
+(setq term-input-ignoredups t)
+;; Original term function constantly asks for my shell
+(defun vj/term ()
+  (interactive)
+  (term shell-file-name))
+;; (evil-set-initial-state 'term-mode 'emacs)
+(global-set-key (kbd "C-z") 'vj/term)
+;; I need to call for disabling evil-mode for both
+;; vj/term and term-char-mode because in first scenario
+;; we should wait until we in *terminal* buffer before
+;; disabling evil
+(advice-add 'vj/term :after #'turn-off-evil-mode)
+(advice-add 'term-char-mode :after #'turn-off-evil-mode)
+(advice-add 'term-line-mode :after #'turn-on-evil-mode)
+
+;; Parens
+(electric-pair-mode t)
+
+;; editorconfig
+(straight-use-package 'editorconfig)
+(editorconfig-mode t)
