94b2a52e08c8651ed380c8cce67001ee5d145dd4
Valentin Ignatev <valentignatev@gmail.com>
2019-01-04 05:47:30 +0300

    Add ed25519 key to keychain

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-eval (keychain --agents ssh --eval --quiet id_rsa)
+eval (keychain --agents ssh --eval --quiet id_rsa id_ed25519)
 alias cdm="cd $HOME/workspace/me"
 alias cdw="cd $HOME/workspace/work"
 set fish_greeting
