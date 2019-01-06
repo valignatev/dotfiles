6a86750412acaec787b2e8ae204da0a88799547b
Valentin Ignatev <valentignatev@gmail.com>
2019-01-06 04:58:45 +0300

    new config from scratch

---
+++ .emacs.d/config/fancification.el
@@ -0,0 +1,37 @@
+(global-hl-line-mode t)
+
+(use-package spacemacs-theme
+  :defer t
+  :custom
+  (spacemacs-theme-comment-bg nil)
+  (spacemacs-theme-comment-italic t))
+;; If I ever want to use original comment color with italics together,
+;; I should uncomment these 2 lines.
+;; See https://github.com/nashamri/spacemacs-theme/issues/104
+;; (custom-set-variables '(spacemacs-theme-custom-colors
+;;                         '((comment-light . "#2aa1ae"))))
+
+(use-package heaven-and-hell
+  :init
+  (setq heaven-and-hell-theme-type 'light)
+  (setq heaven-and-hell-themes
+	'((light . spacemacs-light)
+	  (dark . spacemacs-dark)))
+  :hook (after-init . heaven-and-hell-init-hook)
+  :bind (("C-c <f6>" . heaven-and-hell-load-default-theme)
+	 ("<f6>" . heaven-and-hell-toggle-theme)))
+
+(use-package all-the-icons
+  :config
+  ;; all-the-icons doesn't work without font-lock+
+  ;; And font-lock+ doesn't have autoloads
+  (use-package font-lock+
+    :straight (:host github :repo "emacsmirror/font-lock-plus")
+    :config (require 'font-lock+)))
+
+(use-package all-the-icons-dired
+  :after all-the-icons
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package which-key
+  :init (which-key-mode))
