b0d587110f0579b0797dc692b04f724d0a794882
Valentin Ignatev <valentignatev@gmail.com>
2020-10-02 04:58:59 +0300

    Move stuff unrelated to X11 out of xinitrc, and cut it down generally

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,8 @@
 eval (keychain --agents ssh --eval --quiet id_rsa id_ed25519)
+
+set -x TERMINAL alacritty
+# https://wiki.archlinux.org/index.php/Java#Non-reparenting_window_managers_.2F_Grey_window_.2F_Programs_not_drawing_properly
+set -x _JAVA_AWT_WM_NONREPARENTING 1
+
 alias cdm="cd $HOME/workspace/me"
 alias cdw="cd $HOME/workspace/work"
