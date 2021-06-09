b471ff35b14cd7e77b300489c09e07afb819a8b6
Valentin Ignatev <valentignatev@gmail.com>
2021-06-09 12:04:07 +0300

    Polybar is not bottom 🥺

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,6 @@ killall -q polybar -u ${USER}
 # Wait until the processes have been shut down
 while pgrep -U ${USER} -x polybar >/dev/null;do sleep 1; done
 
-MONITOR=eDP-1 polybar top &
+MONITOR=eDP-1 polybar bottom &
 
 echo "Polybar launched..."
