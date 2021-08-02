dfa0100efb17535c88574c213b6fb1c546d703ac
Valentin Ignatev <valentignatev@gmail.com>
2021-08-02 13:27:09 +0300

    Emacs went to its own repository
    Because I started using windows for programming :woozy:

--- .config/emacs/early-init.el
+++
@@ -1,9 +0,0 @@
-;; -*- lexical-binding: t; -*-
-
-(setq gc-cons-threshold most-positive-fixnum)
-
-;; Disable package.el
-(setq package-enable-at-startup nil)
-(advice-add #'package--ensure-init-file :override #'ignore)
-
-(setq frame-inhibit-implied-resize t)
